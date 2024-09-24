b3fcc0e5021683e9cf4267f2a15a5073fa1ce964
KietLe <letuankiet26102003kl@gmail.com>
2024-09-24 14:01:02 +0700

    bo sung bai 2

--- app/src/main/java/com/example/a2124802010277_letuankiet_bth02/BT6Edit_View_Buttom.java
+++ app/src/main/java/com/example/a2124802010277_letuankiet_bth02/BT6Edit_View_Buttom.java
@@ -15,11 +15,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class BT6Edit_View_Buttom extends AppCompatActivity {
-    Button changeText;
-    TextView simpleTextView;
-    private Button btnReg;
-    private TextView txtShowMessage;
-    private EditText edtName, edtPassword, edtBirthday, edtEmail;
+
+    EditText edtSolarYear;
+    Button btnConvert;
+    TextView txtLunarYear;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,35 +26,19 @@ public class BT6Edit_View_Buttom extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_bt6_edit_view_buttom);
 
-        //Cấu hình biến
-        changeText=findViewById(R.id.btnChangeText);
-        simpleTextView=findViewById(R.id.simpleTextView);
-
-
-        //Xử lý
-        changeText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                simpleTextView.setText("Sau khi click vào");
-            }
-        });
-        edtName		=	(EditText)findViewById(R.id.edtName);
-        edtPassword	=	(EditText)findViewById(R.id.edtPassword);
-        edtBirthday	=	(EditText)findViewById(R.id.edtBirthday);
-        edtEmail	=	(EditText)findViewById(R.id.edtEmail);
-        txtShowMessage=	(TextView)findViewById(R.id.txtShowMessage);
-        btnReg		=	(Button)findViewById(R.id.btnReg);
-        btnReg.setOnClickListener(new View.OnClickListener() {
+        edtSolarYear=findViewById(R.id.edtSolarYear);
+        btnConvert=findViewById(R.id.btnConvert);
+        txtLunarYear=findViewById(R.id.txtLunarYear);
+        String[] can={"Canh","Tân","Nhâm","Quý","Giáp","Ất","Bính","Đinh","Mậu","Kỷ"};
+        String[] chi={"Thân","Dậu","Tuất","Hợi","Tý","Sửu","Dần","Mão","Thìn","Tỵ","Ngọ","Mùi"};
+        btnConvert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                String str="Thông tin tài khoản \n";
-                str+="Tài Khoản:"+edtName.getText().toString()+"\n Mật khẩu:"+edtPassword.getText().toString();
-                str+="\nNgày sinh: "+edtBirthday.getText().toString()+"\nEmail:"+edtEmail.getText().toString();
-                txtShowMessage.setText(str);
-                txtShowMessage.setBackgroundColor(Color.GREEN);
+                Integer Year=Integer.parseInt(edtSolarYear.getText().toString());
+                Integer canI=Year%10;
+                Integer chiI=Year%12;
+                txtLunarYear.setText(can[canI]+" "+chi[chiI]);
             }
         });
-
     }
 }
